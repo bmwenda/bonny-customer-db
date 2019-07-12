44a612bfdc27beec45f3b68a78eede7980e655ba
Boniface Mwenda <boniface.mwenda@andela.com>
2019-07-12 14:39:04 +0300

    Implement query filtering

--- app/controllers/v1/customers_controller.rb
+++ app/controllers/v1/customers_controller.rb
@@ -2,12 +2,23 @@ module V1
   class CustomersController < ApplicationController
     def index
       @customers = Customer.paginate(page: params[:page]).order('id')
+      @customers = filter(@customers)
       render :index, status: :ok
     end
 
     def create
       SaveCustomersJob.perform_later
       render :create, status: :created
+    end
+
+    private
+
+    def filter(customers)
+      customers.where(filter_params)
+    end
+
+    def filter_params
+      params.permit(:age, :gender, :region, :job_classification)
     end
   end
 end

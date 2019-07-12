fa9c3d44a80d9ada07e3373dc8f324b1c0851969
Boniface Mwenda <boniface.mwenda@andela.com>
2019-07-12 12:34:39 +0300

    Use file parser module

--- app/jobs/save_customers_job.rb
+++ app/jobs/save_customers_job.rb
@@ -2,17 +2,9 @@ class SaveCustomersJob < ApplicationJob
   queue_as :default
 
   def perform
-    parsed_data.each do |data|
+    FileParser.parse.each do |data|
       customer = Customer.new(data)
       customer.save
     end
-  end
-
-  private
-
-  def parsed_data
-    csv_data = Parsers::CSVParser.new.parse
-    excel_data = Parsers::ExcelParser.new.parse
-    csv_data + excel_data
   end
 end
--- app/lib/file_parser.rb
+++ app/lib/file_parser.rb
@@ -1,5 +1,10 @@
 class FileParser
-  def parse(parser)
-    parser.parse
+  PARSERS = %w[Parsers::CSVParser Parsers::ExcelParser].freeze
+  def self.parse
+    file_data = []
+    PARSERS.each do |parser|
+      file_data << parser.constantize.new.parse
+    end
+    file_data.flatten
   end
 end
